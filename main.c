e69f797a6bf6f9122461a25eb29588045400f811
hwaninging <hwanhee@sookmyung.ac.kr>
2025-12-04 15:45:15 +0900

    실습 3: 구조체에 대한 동적 할당
    book(1,3) : 5 C++ Programming
    book(2,4) : 3 Communications Theory

--- main.c
+++ main.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-//2. 구조체에 대한 동적 할당 
-
+//3. 구조체에 대한 동적 할당 
+//이중 구조의 자료형을 동적 할당으로 만들어보자 
 struct Book {
        int number;
        char title[10];
        }; 
 int main(int argc, char *argv[])
 {
-  struct Book *p;
+  int i; 
+  struct Book **bookshelf; 
   
-  p = (struct Book*)malloc(2*sizeof(struct Book));
-  
-  if (p == NULL){
-        printf("메모리 할당 오류\n");
-        return;
-        }
+  //3개의 Book 구조체 포인터 변수 동적 할당 
+  bookshelf = (struct Book**)malloc(3*sizeof(struct Book*)); //책장에 1,2,3층 완성 
+  //10개의 Book 구조체 변수 동적 할당 
+  for(i=0;i<3;i++)
+     bookshelf[i] = (struct Book*)malloc(10*sizeof(struct Book)); 
    
-  p -> number = 1;
-  strcpy(p->title, "C Programming");
+  bookshelf[1][3].number = 5; //3층 중 1층, 3번째 책에 넘버랑 타이틀 적어  
+  strcpy(bookshelf[1][3].title, "C++ Programming");
   
-  (p+1)->number = 2;
-  strcpy((p+1)->title, "Electronics");
+  (bookshelf[2]+4)->number = 3;
+  strcpy((bookshelf[2]+4)->title, "Communications Theory"); 
   
-  free(p); 
+  printf("book(1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+  printf("book(2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title); 
+
+  //2차원 구조체 메모리 반납 (사용했으니까) : 역순으로 free 시키면 됨. 
+  for(i=0;i<3;i++)
+     free(bookshelf[i]);
+  free(bookshelf);
       
   system("PAUSE");
   return;	
